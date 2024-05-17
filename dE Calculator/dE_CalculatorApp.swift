53b16d9ba0c2dfdca3557815b8ca1af6afd3daa5
DenDmitriev <dv.denstr@gmail.com>
2024-05-17 09:24:10 +0300

    Initial Commit

---
+++ dE Calculator/dE_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  dE_CalculatorApp.swift
+//  dE Calculator
+//
+//  Created by Denis Dmitriev on 17.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct dE_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

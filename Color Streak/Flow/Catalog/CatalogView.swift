f8e8b21768efc7e8760aa0e2d09c927767c140f4
DenDmitriev <dv.denstr@gmail.com>
2024-06-24 15:22:38 +0300

    Update CatalogView.swift

--- Color Streak/Flow/Catalog/CatalogView.swift
+++ Color Streak/Flow/Catalog/CatalogView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import UniformTypeIdentifiers
+import FirebaseAnalytics
 
 struct CatalogView: View {
     @EnvironmentObject private var coordinator: Coordinator<HomeRouter, HomeError>
